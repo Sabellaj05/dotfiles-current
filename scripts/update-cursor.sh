679243a3da629631fb7cc5915ab595e1b8f7aaa1
saens <sabellamanjuan@gmail.com>
2025-03-07 02:31:58 -0300

    changed name to cursor launcher, link to auto-update is old, did it manually

--- scripts/update-cursor.sh
+++ scripts/update-cursor.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 APPDIR=/opt
+APPNAME="cursor.AppImage"
 APPIMAGE_URL="https://downloader.cursor.sh/linux/appImage/x64"
+sudo rm $APPDIR/$APPNAME
 
-wget -O $APPDIR/cursor.appimage $APPIMAGE_URL
-chmod +x $APPDIR/cursor.appimage
+wget -O $APPDIR/cursor.AppImage $APPIMAGE_URL
+chmod +x $APPDIR/cursor.AppImage
